40fe98bd72af7d89a2d8b08a345163799b822e42
GabrielH-NH <gabrielhenriquenh@gmail.com>
2023-05-24 20:58:53 -0300

    aprendendo sobre variaveis

---
+++ variaveis.js
@@ -0,0 +1,66 @@
+//var nome = "Gabriel"
+//let idade = 23
+//const possuiFaculdade = false
+
+
+// Evitam repetições
+
+var preco = 25
+var totalComprado = 5
+var totalPreco = totalComprado * preco
+
+console.log(totalPreco) // resultado 125
+
+/*--------------------------------------------------------------*/
+
+// Virgula, usada para criar mais de uma variavel sem precisar repetir a palavra chave var
+
+var nome = 'Gabriel', cidade = 'Novo Hamburgo'
+
+console.log(nome, cidade) // resultado Gabriel Novo Hamburgo
+
+/*--------------------------------------------------------------*/
+
+// Sem definir valor
+
+var semDefinir; // retorna undefined
+
+/*--------------------------------------------------------------*/
+
+// Declarar
+
+console.log(nome); // retorna nome is not defined
+
+/*--------------------------------------------------------------*/
+
+// Hoisting
+
+console.log(nome);
+var nome = 'André';
+// Retorna undefined
+
+var profissao = 'Designer';
+console.log(profissao);
+// Retornar Designer
+
+
+/*--------------------------------------------------------------*/
+
+//EXERCICIOS
+
+// Declarar uma variável com o seu nome
+var nome = "Gabriel"
+// Declarar uma variável com a sua idade
+let idade = 23
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza"
+// Declarar 5 variáveis diferentes sem valores
+var nome;
+var sobrenome;
+var possuiEmprego;
+var time;
+var numero;
+
